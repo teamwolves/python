5b03086f68e2299ce92d2e0a75c4f82f5e919fc0
santharaam <santharaam@gmail.com>
2018-12-02 09:54:23 -0600

    no message

---
+++ programs/pg09_strip.py
@@ -0,0 +1,7 @@
+#The strip() method removes any whitespace from the beginning or the end:
+
+myString = "    Skyzone is awesome       "
+
+print(myString)
+
+print(myString.strip())
---
+++ programs/pg10_length.py
@@ -0,0 +1,5 @@
+#The len() method returns the length of a string:
+
+myString = "Skyzone is awesome"
+
+print(len(myString));
